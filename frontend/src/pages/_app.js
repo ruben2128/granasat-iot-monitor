a246fa71d36a5bb5a27fa4b441cf1d923589f990
ruben2128 <gilrubenmartin@gmail.com>
2026-09-01 15:17:20 +0200

    Fixes Pantallas Titular

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,8 +1,8 @@
 import "@/styles/globals.css";
 
 // Versión y fecha de última modificación
-const VERSION = 'v2.0';
-const FECHA = '16/07/2026';
+const VERSION = 'v2.0.1';
+const FECHA = '26/08/2026';
 
 export default function App({ Component, pageProps }) {
   return (
